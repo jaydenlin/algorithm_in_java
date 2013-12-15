0ea61cc36bd3edc5e883f415f8a70fb13a6e5f54
JaydenLin <j5905653@gmail.com>
2013-12-15 22:23:09 +0800

    oneD done

--- src/polynomial.java
+++ src/polynomial.java
@@ -1,13 +1,26 @@
-
 public class polynomial {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// 2x^3 + 2x^2 + 5x^1 + 7x^0
+	private static double coefficients1D[] = { 2, 2, 5, 7 };
 
+	public static void main(String[] args) {
+
+		System.out.println(OneD(coefficients1D, 1.0));// 16
 	}
-	
-	
-	
-	
+
+	private static double OneD(double[] coefficients, double xValue) {
+		double result = 0.0;
+		for (int i = 0; i < coefficients.length; i++) {
+
+			result = result * xValue + coefficients[i];
+
+		}
+		return result;
+	}
+
+	private static double TwoD(double coefficients,double xvalue,double yValue) {
+
+		return 1.0;
+	}
 
 }
