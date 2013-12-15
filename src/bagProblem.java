945933281ad0ce54f8bf9f55874e6a4521788623
JaydenLin <j5905653@gmail.com>
2013-12-16 01:41:24 +0800

    add tree solution comment

--- src/bagProblem.java
+++ src/bagProblem.java
@@ -7,7 +7,7 @@ public class bagProblem {
 	static int[] weight = { 2, 1, 3, 2 };
 	static int[] value = { 3, 2, 4, 2 };
 	static int limitWeight = 5;
-	static int sumValue = 0;//outside value
+	static int sumValue = 0;//outside value 樹狀結構的[暫時解]通常存在外部
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		carry(0, 0);
@@ -19,7 +19,7 @@ public class bagProblem {
 		if (itemIndex == itemNumber) {
 			System.out.println(sumValue);
 		} else if ((weight[itemIndex] + sumWeight) <= limitWeight) {
-			sumValue = Math.max(sumValue, sumValue + value[itemIndex]);//save the greater one to outside
+			sumValue = Math.max(sumValue, sumValue + value[itemIndex]);//save the greater one to outside 比較每個[葉]
 			carry(itemIndex + 1, sumWeight + weight[itemIndex]);
 		} else {
 			carry(itemIndex + 1, sumWeight);
