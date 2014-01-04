b918d069c14427670f2979eded9ea7a4edec8878
JaydenLin <j5905653@gmail.com>
2014-01-04 16:35:51 +0800

    bubble and selection sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,37 @@
+
+public class BubbleSort {
+
+	/**
+	 * @param args
+	 */
+	static int[] a={3,14,1,34,23,56,34,33,89,23,12};
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		bubbleSort(a);
+	}
+	
+	public static void bubbleSort(int[] a) {
+		
+		for(int i=0;i<a.length;i++){
+			
+			for(int k=0;k<a.length;k++){
+				if(a[i]<a[k]){//注意等號，由小到大排時，當比到小的時候要被換到前面去
+					int temp=a[i];
+					a[i]=a[k];
+					a[k]=temp;
+				}
+			}
+			
+		}
+		
+		for(int j=0;j<a.length;j++){
+			System.out.print(a[j]+",");
+		}
+		
+	}
+	
+
+}
---
+++ src/SelectionSort.java
@@ -0,0 +1,46 @@
+
+public class SelectionSort {
+
+	/**
+	 * @param args
+	 */
+	static int[] a={3,14,1,34,23,56,34,33,89,23,12};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		selectionSort(a);
+
+	}
+	
+	public static void selectionSort(int[] a) {
+		int index=0;//保存最小值的index
+		for(int i=0;i<a.length;i++){
+			
+			//for迴圈的變形，當有可能會超過範圍時用while
+			int k=i+1;
+			while(k<a.length){
+				
+				if(a[k]<a[index]){
+					
+					index=k;
+				}
+				
+				k++;
+			}
+			
+			//switch
+			int temp=a[i];
+			a[i]=a[index];
+			a[index]=temp;
+			
+			
+		}
+		
+		
+		
+		for(int j=0;j<a.length;j++){
+			System.out.print(a[j]+",");
+		}
+	}
+	
+}
